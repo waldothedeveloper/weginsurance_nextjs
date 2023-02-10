a213c5f2f3e07398406dbcae53007540363d410b
Waldo Lavaut <waldothedeveloper@gmail.com>
2023-02-09 21:26:06 -0500

    Tailwindcss installed ok, and yarn was changed for npm

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>WEG INSURANCE</div>
+      <main>
+        <h1 className="text-2xl font-bold text-blue-500">TailwindCSS ok!</h1>
+      </main>
     </>
   );
 }
