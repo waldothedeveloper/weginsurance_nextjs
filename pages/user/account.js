c97387ca21b7e5da6169987dade81e6d3e6bb0af
Waldo Lavaut <waldothedeveloper@gmail.com>
2023-02-12 23:40:20 -0500

    Added a simple admin dashboard UI

--- pages/user/account.js
+++ pages/user/account.js
@@ -1,13 +1,15 @@
-import Logout from "@/components/logout";
+import { AppShell } from "@/components/AppShell/AppShell";
+// import Logout from "@/components/logout";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
+//
 export default function UserAccount() {
   const { user } = useUser();
   return (
     <div>
-      <div>Hola, {user?.name}</div>
-      <Logout />
+      <AppShell />
+      {/* <Logout /> */}
     </div>
   );
 }
