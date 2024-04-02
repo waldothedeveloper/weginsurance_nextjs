6f9df9270b27ad4fcfe420f84195c814f6b23e36
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-04-01 23:30:28 -0400

    chore: deleted unused file, added global error for support with Sentry

--- app/documents/error.tsx
+++ app/documents/error.tsx
@@ -1,4 +1,6 @@
-'use client' // Error components must be Client Components
+'use client'
+
+import * as Sentry from "@sentry/nextjs";
 
 import { useEffect } from 'react'
 
@@ -10,20 +12,22 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
+    // Log the error to Sentry
+    Sentry.captureException(error);
     // Log the error to an error reporting service
     console.error(error)
   }, [error])
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>Lo sentimos, ha ocurrido un error!</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
-        Try again
+        Intentelo nuevamente.
       </button>
     </div>
   )
