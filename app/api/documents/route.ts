96ff730e3bd8acf1a07125bb7aa5554b74a3cceb
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-04-22 23:35:50 -0400

    fix: using the edge serverless fn isntead

--- app/api/documents/route.ts
+++ app/api/documents/route.ts
@@ -1,3 +1,6 @@
+export const runtime = "edge";
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
+
 export async function GET(request: Request) {
   const requestHeaders = new Headers(request.headers);
   const { searchParams } = new URL(request.url);
