6dabdf7546fb4a1619cca97d9d4d0d7ecf631bd8
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-07-14 22:03:20 -0400

    chore: layout added with improvements to some client vs server pages

--- app/(routes)/users/create-user/page.tsx
+++ app/(routes)/users/create-user/page.tsx
@@ -1,4 +1,5 @@
 import { BankInfo } from "./_components/bank-info";
+import CreateUserHeader from "./_components/create-user-header";
 import { Divider } from "./_components/divider";
 import { InsuranceInfo } from "./_components/insurance-info";
 import { Notes } from "./_components/notes";
@@ -7,18 +8,22 @@ import { WorkInfo } from "./_components/work-info";
 
 export default function CreateUser() {
   return (
-    <div className="flex flex-col justify-center items-start size-full max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 lg:py-12">
-      <form>
-        <PersonalInfo />
-        <Divider />
-        <BankInfo />
-        <Divider />
-        <WorkInfo />
-        <Divider />
-        <InsuranceInfo />
-        <Divider />
-        <Notes />
-      </form>
-    </div>
+    <>
+      <CreateUserHeader />
+
+      <div className="border-t border-gray-100 mt-6 flex flex-col justify-center items-start size-full max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 lg:py-12">
+        <form>
+          <PersonalInfo />
+          <Divider />
+          <BankInfo />
+          <Divider />
+          <WorkInfo />
+          <Divider />
+          <InsuranceInfo />
+          <Divider />
+          <Notes />
+        </form>
+      </div>
+    </>
   );
 }
---
+++ app/(routes)/users/layout.tsx
@@ -0,0 +1,35 @@
+import { DesktopNavigation } from "@/components/ui/desktop-navigation";
+import { MobileSidebarNavigation } from "@/components/ui/mobile-sidebar-navigation";
+import { SearchUsers } from "./_components/search-users";
+import { UserProvider } from "./_hooks/useUser";
+import UserSkeleton from "../users/_components/user-skeleton";
+import { UsersList } from "../users/_components/users-list";
+export default function UsersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {/* The UserProvider is a client component passing the selected user information down  */}
+      <UserProvider>
+        <MobileSidebarNavigation />
+        <DesktopNavigation />
+        {/* Main column which is used for main operations like chat, create new user, etc */}
+        <main className="lg:pl-72">
+          <div className="xl:pl-96">
+            <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">{children}</div>
+          </div>
+        </main>
+
+        {/* Secondary column (hidden on smaller screens) */}
+        <aside className="fixed inset-y-0 left-72 hidden w-96 border-r border-gray-200 xl:block h-screen overflow-hidden">
+          <SearchUsers />
+          <UsersList>
+            <UserSkeleton />
+          </UsersList>
+        </aside>
+      </UserProvider>
+    </>
+  );
+}
