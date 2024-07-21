38794e42b520d64326d68b61c54f7467035ddf16
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-07-20 21:39:47 -0400

    removed eslint.json

--- eslintrc.cjs
+++ eslintrc.cjs
@@ -1,7 +1,15 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "next",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
+  ignores: ["/functions/*"],
+  env: { es6: true, jest: true },
 };
