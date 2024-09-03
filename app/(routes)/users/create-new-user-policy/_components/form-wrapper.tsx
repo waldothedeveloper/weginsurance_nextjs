accf99c5a1e820b3ae3e3fe8c70ec90d7ebe4141
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-09-03 06:34:23 -0400

    wizard now can create additional dynamic steps

--- app/(routes)/users/create-new-user-policy/_components/form-wrapper.tsx
+++ app/(routes)/users/create-new-user-policy/_components/form-wrapper.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 
+import { AddMoreDependantsDialog } from "./add-more-dependants-dialog";
 import { BankInfo } from "./bank-info";
 import { Divider } from "./divider";
 import { HeadStepper } from "./head-stepper";
@@ -11,63 +13,94 @@ import { Notes } from "./notes";
 import PersonalInfo from "./personal-info";
 import { Stepper } from "./footer-stepper";
 import { WorkInfo } from "./work-info";
-// import CreateUserHeader from "./create-user-header";
 import { useCreateUserPolicy } from "../hooks/useCreateUserPolicy";
 
 export const FormWrapper = () => {
-  const { steps, handleNextStep, handlePreviousStep } = useCreateUserPolicy();
+  const [openAdditionalDependantsDialog, setOpenAdditionalDependantDialog] =
+    useState(false);
+  const [addMoreDependants, setAddMoreDependants] = useState(false);
+  const [userEventDispatch, setUserEventDispatch] = useState<string>("");
+  const { steps, dispatchSteps } = useCreateUserPolicy();
+  console.log("steps: ", steps);
   const currStep = steps.find(
     (step: CreateNewUserPolicyMultiStepForm) => step.status === "current"
-  );
-  console.log('currStep: ', currStep);
+  )!;
+
+  // create one more step for the additional dependants
+  useEffect(() => {
+    if (addMoreDependants) {
+      dispatchSteps({
+        type: "additional_dependants",
+        data: {},
+        stepNumber: currStep.id ?? steps.length,
+      });
+      setAddMoreDependants(false);
+    }
+
+    return () => setAddMoreDependants(false);
+  }, [addMoreDependants]);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<UserPolicyInputs>();
-  console.log("errors: ", errors);
+
   const onSubmit: SubmitHandler<UserPolicyInputs> = (data) => {
-    // console.log(`data`, data);
-    handleNextStep();
+    if (currStep.id === steps.length - 1 && userEventDispatch !== "previous") {
+      console.log(`is this the last step? ${currStep.id === steps.length}`);
+      setOpenAdditionalDependantDialog(true);
+    } else {
+      dispatchSteps({
+        type: userEventDispatch,
+        data: data ?? {},
+        stepNumber: currStep.id ?? steps.length,
+      });
+    }
   };
+
   return (
-    <>
-      {/* <CreateUserHeader /> */}
-      <HeadStepper currStep={currStep} handlePreviousStep={handlePreviousStep} handleNextStep={handleSubmit(onSubmit)} />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <HeadStepper
+        currStep={currStep}
+        setUserEventDispatch={setUserEventDispatch}
+      />
       <div className="border-t border-gray-100 mt-6 flex flex-col justify-center items-start size-full max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 lg:py-12">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <PersonalInfo register={register} />
-          <Divider />
-          <LegalStatus />
-          <Divider />
-          <BankInfo />
-          <Divider />
-          <WorkInfo />
-          <Divider />
-          <InsuranceInfo />
-          <Divider />
-          <Notes />
-          <Divider />
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button
-              type="button"
-              onClick={handlePreviousStep}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Anterior
-            </button>
-            <button
-              onClick={handleSubmit(onSubmit)}
-              type="button"
-              className="inline-flex justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-            >
-              {currStep?.id === 3 ? "Finalizar" : "Siguiente"}
-            </button>
-          </div>
-          <Stepper steps={steps} />
-        </form>
+        <PersonalInfo register={register} />
+        <Divider />
+        <LegalStatus />
+        <Divider />
+        <BankInfo />
+        <Divider />
+        <WorkInfo />
+        <Divider />
+        <InsuranceInfo />
+        <Divider />
+        <Notes />
+        <Divider />
+        <div className="mt-6 flex items-center justify-end space-x-3">
+          <button
+            type="submit"
+            onClick={() => setUserEventDispatch("previous")}
+            className="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            Anterior
+          </button>
+          <button
+            onClick={() => setUserEventDispatch("next")}
+            type="submit"
+            className="inline-flex justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >
+            {currStep.id === 3 ? "Finalizar" : "Continuar"}
+          </button>
+        </div>
+        <Stepper steps={steps} />
       </div>
-    </>
+      <AddMoreDependantsDialog
+        open={openAdditionalDependantsDialog}
+        setOpen={setOpenAdditionalDependantDialog}
+        addMoreDependants={setAddMoreDependants}
+      />
+    </form>
   );
 };
--- app/(routes)/users/create-new-user-policy/_components/head-stepper.tsx
+++ app/(routes)/users/create-new-user-policy/_components/head-stepper.tsx
@@ -1,23 +1,21 @@
 export const HeadStepper = ({
   currStep,
-  handlePreviousStep,
-  handleNextStep,
+  setUserEventDispatch,
 }: {
   currStep: CreateNewUserPolicyMultiStepForm;
-  handleNextStep: () => void;
-  handlePreviousStep: () => void;
+  setUserEventDispatch: React.Dispatch<React.SetStateAction<string>>;
 }) => {
   return (
     <div className="md:flex md:items-center md:justify-between md:space-x-5">
       <div className="flex items-start space-x-5">
-        <div className="flex-shrink-0">{currStep?.icon}</div>
+        <div className="flex-shrink-0">{currStep.icon}</div>
         {/*
           Use vertical padding to simulate center alignment when both lines of text are one line,
           but preserve the same layout if the text wraps without making the image jump around.
         */}
         <div className="pt-1.5">
           <h1 className="text-2xl font-bold text-gray-900">
-            Paso {currStep.id}: {` `}
+            Paso {currStep.id + 1}: {` `}
             {currStep.title}
           </h1>
           <p className="text-sm font-medium text-gray-500">
@@ -27,18 +25,18 @@ export const HeadStepper = ({
       </div>
       <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
         <button
-          type="button"
-          onClick={handlePreviousStep}
+          onClick={() => setUserEventDispatch("previous")}
+          type="submit"
           className="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
         >
           Anterior
         </button>
         <button
-          type="button"
-          onClick={handleNextStep}
+          onClick={() => setUserEventDispatch("next")}
+          type="submit"
           className="inline-flex items-center justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
         >
-          {currStep?.id === 3 ? "Finalizar" : "Siguiente"}
+          {currStep.id === 2 ? "Finalizar" : "Continuar"}
         </button>
       </div>
     </div>
