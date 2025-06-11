048bae28e0b8c58d10b97374662b42cec34efc3e
Waldo Lavaut <waldothedeveloper@gmail.com>
2025-06-11 12:36:29 -0400

    buttons have been fixed

---
+++ app/(routes)/(shared)/users/_components/user-profile/profile-header.tsx
@@ -0,0 +1,79 @@
+import {
+  DocumentTextIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+
+import Image from "next/image";
+import { UserContext } from "../../../../../global-hooks/useUser";
+import { createAvatarImage } from "@/appUtils/create-avatar";
+import { formatPhoneNumberToNationalUSAformat } from "@/utils/formatPhoneNumber";
+import { useContext } from "react";
+
+//
+export const ProfileHeader = () => {
+  const { selectedUser } = useContext(UserContext);
+  return selectedUser ? (
+    <div className="md:flex md:items-center md:justify-between md:space-x-5 px-6">
+      <div className="flex items-start space-x-5">
+        <div className="shrink-0">
+          <div className="relative">
+            <Image
+              width={120}
+              height={120}
+              src={
+                selectedUser?.user.personal_info.avatar ||
+                createAvatarImage(selectedUser?.user?.personal_info)
+              }
+              className="size-16 rounded-full"
+              alt="User Avatar"
+            />
+
+            <span
+              aria-hidden="true"
+              className="absolute inset-0 rounded-full shadow-inner"
+            />
+          </div>
+        </div>
+        {/*
+          Use vertical padding to simulate center alignment when both lines of text are one line,
+          but preserve the same layout if the text wraps without making the image jump around.
+        */}
+        <div className="pt-1.5">
+          <h1 className="text-2xl font-bold text-gray-900">
+            {selectedUser?.user.personal_info.firstname}{" "}
+            {selectedUser?.user.personal_info.lastname}
+          </h1>
+          <p className="text-sm font-medium text-gray-500">
+            {formatPhoneNumberToNationalUSAformat(
+              selectedUser?.user.personal_info.phone
+            )}
+          </p>
+        </div>
+      </div>
+      <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-y-0 sm:space-x-3 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
+        <button
+          type="button"
+          className="inline-flex gap-x-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50"
+        >
+          <DocumentTextIcon aria-hidden="true" className="-ml-0.5 size-5" />
+          Crear PDF
+        </button>
+        <button
+          type="button"
+          className="inline-flex gap-x-2 rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs ring-1 ring-red-300 ring-inset hover:bg-red-400"
+        >
+          <TrashIcon aria-hidden="true" className="-ml-0.5 size-5" />
+          Eliminar
+        </button>
+        <button
+          type="button"
+          className="inline-flex items-center gap-x-2 rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+        >
+          <PencilSquareIcon aria-hidden="true" className="-ml-0.5 size-5" />
+          Editar
+        </button>
+      </div>
+    </div>
+  ) : null;
+};
--- app/(routes)/(shared)/users/_components/user-profile/profile.tsx
+++ app/(routes)/(shared)/users/_components/user-profile/profile.tsx
@@ -1,17 +1,11 @@
 "use client";
 
-import {
-  DocumentTextIcon,
-  PencilSquareIcon,
-  TrashIcon,
-} from "@heroicons/react/20/solid";
 import { useContext, useState } from "react";
 
-import Image from "next/image";
 import NoUserSelected from "./no-user-selected";
+import { ProfileHeader } from "./profile-header";
 import { UserContext } from "../../../../../global-hooks/useUser";
 import { classNames } from "@/utils/classNames";
-import { createAvatarImage } from "@/appUtils/create-avatar";
 import dynamic from "next/dynamic";
 
 // TODO: Implement React.lazy with Suspense instead of dynamic imports because it will give a nice loading state instead of just a blank page for these components
@@ -34,71 +28,7 @@ export const UserProfile = () => {
   ]);
   return selectedUser ? (
     <article>
-      <div className="h-32 w-full lg:h-48 bg-linear-to-r from-blue-50 to-cyan-100"></div>
-      <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-        <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-          <div className="rounded-full ring-4 ring-gray-100 sm:size-32 size-24 relative bg-gray-50">
-            <div className="rounded-full ring-4 ring-gray-100 sm:size-32 size-24 relative bg-gray-50">
-              {selectedUser && (
-                <Image
-                  fill
-                  src={
-                    selectedUser.user.personal_info.avatar ||
-                    createAvatarImage(selectedUser.user.personal_info)
-                  }
-                  className="rounded-full shadow-md"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  alt="User Avatar"
-                />
-              )}
-            </div>
-            <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-              <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
-                <h1 className="truncate text-2xl font-bold text-gray-900">
-                  {selectedUser.user.personal_info.firstname}
-                </h1>
-              </div>
-              <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
-                <button
-                  type="button"
-                  className="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <DocumentTextIcon
-                    aria-hidden="true"
-                    className="-ml-0.5 h-5 w-5 text-gray-400"
-                  />
-                  PDF
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <PencilSquareIcon
-                    aria-hidden="true"
-                    className="-ml-0.5 h-5 w-5 text-gray-400"
-                  />
-                  Editar
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <TrashIcon
-                    aria-hidden="true"
-                    className="-ml-0.5 h-5 w-5 text-gray-400"
-                  />
-                  Eliminar
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
-            <h1 className="truncate text-2xl font-bold text-gray-900">
-              {selectedUser.user.personal_info.firstname}
-            </h1>
-          </div>
-        </div>
-      </div>
+      <ProfileHeader />
 
       {/* Tabs */}
       <div className="mt-6 sm:mt-2 2xl:mt-5">
